784504d6b0bf7fb3b3e8204dffd294c30e303ae9
MaxXSoft <x@MaxXSoft.net>
2023-11-17 16:19:36 +0800

    (lib) added macro `build_assert_ne`

--- src/lib.rs
+++ src/lib.rs
@@ -83,6 +83,37 @@ macro_rules! build_assert_eq {
   };
 }
 
+#[macro_export]
+macro_rules! build_assert_ne {
+  ($left:expr, $right:expr $(,)?) => {
+    match (&$left, &$right) {
+      (left_val, right_val) => {
+        if *left_val == *right_val {
+          $crate::build_error!(
+            "assertion `left != right` failed\n  left: {:?}\n right: {:?}",
+            &*left_val,
+            &*right_val,
+          );
+        }
+      }
+    }
+  };
+  ($left:expr, $right:expr, $($arg:tt)+) => {
+    match (&$left, &$right) {
+      (left_val, right_val) => {
+        if *left_val == *right_val {
+          $crate::build_error!(
+            "assertion `left != right` failed: {}\n  left: {:?}\n right: {:?}",
+            format!($($arg)+),
+            &*left_val,
+            &*right_val,
+          );
+        }
+      }
+    }
+  };
+}
+
 #[cfg(test)]
 mod tests {
   #[test]
@@ -136,8 +167,40 @@ mod tests {
 
   #[cfg(debug_assertions)]
   #[test]
-  #[should_panic(expected = "assertion `left == right` failed: A must be equal to B, got 1 and 2\n  left: 1\n right: 2")]
+  #[should_panic(
+    expected = "assertion `left == right` failed: A must be equal to B, got 1 and 2\n  left: 1\n right: 2"
+  )]
   fn test_assert_const_eq_fail() {
     assert_const_eq::<1, 2>();
+  }
+
+  #[test]
+  fn test_build_assert_ne() {
+    build_assert_ne!(1, 2);
+  }
+
+  #[cfg(debug_assertions)]
+  #[test]
+  #[should_panic(expected = "assertion `left != right` failed\n  left: 1\n right: 1")]
+  fn test_build_assert_ne_fail() {
+    build_assert_ne!(1, 1);
+  }
+
+  fn assert_const_ne<const A: usize, const B: usize>() {
+    build_assert_ne!(A, B, "A must not be equal to B, got {} and {}", A, B);
+  }
+
+  #[test]
+  fn test_assert_const_ne() {
+    assert_const_ne::<1, 2>();
+  }
+
+  #[cfg(debug_assertions)]
+  #[test]
+  #[should_panic(
+    expected = "assertion `left != right` failed: A must not be equal to B, got 1 and 1\n  left: 1\n right: 1"
+  )]
+  fn test_assert_const_ne_fail() {
+    assert_const_ne::<1, 1>();
   }
 }
